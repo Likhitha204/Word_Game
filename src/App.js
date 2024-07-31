726448166bdc240cacfc9c9e47e1044e8d881e70
Likhitha-204 <blikhitha204@gmail.com>
2024-07-31 14:40:08 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -8,20 +8,20 @@ const sampleWords = [
     description: "The Tag used to include javascript code in HTML."
   },
   {
-      word: "COLOR",
-      description: "CSS property used to change text color of an element."
+    word: "COLOR",
+    description: "CSS property used to change text color of an element."
   },
   {
-      word: "STATIC",
-      description: "Default value of the position property in CSS."
+    word: "STATIC",
+    description: "Default value of the position property in CSS."
   },
   {
-      word: "STRONG",
-      description: "Correct HTML element to define important text."
+    word: "STRONG",
+    description: "Correct HTML element to define important text."
   },
   {
-      word: "ORDEREDLIST",
-      description: "How can you make a numbered list?"
+    word: "ORDEREDLIST",
+    description: "How can you make a numbered list?"
   },
   {
     word: "UNORDEREDLIST",
@@ -35,159 +35,163 @@ const sampleWords = [
     word: "EVENT",
     description: "Onblur and OnFocus are ____ Attributes."
   }
-
 ];
 
 const getRandomWord = () => {
-    const randomPlace = Math.floor(Math.random() * sampleWords.length);
-    return sampleWords[randomPlace];
+  const randomPlace = Math.floor(Math.random() * sampleWords.length);
+  return sampleWords[randomPlace];
 };
 
 const GFGWordGame = () => {
-    const [wordData, setWordData] = useState(getRandomWord());
-    const [msg, setMsg] = useState("");
-    const [chosenLetters, setChosenLetters] = useState([]);
-    const [hints, setHints] = useState(3);
-    const [displayWord, setDisplayWord] = useState(false);
-    const [gameOver, setGameOver] = useState(false);
-    const [wrongGuesses, setWrongGuesses] = useState(0);
+  const [wordData, setWordData] = useState(getRandomWord());
+  const [msg, setMsg] = useState("");
+  const [chosenLetters, setChosenLetters] = useState([]);
+  const [hints, setHints] = useState(3);
+  const [displayWord, setDisplayWord] = useState(false);
+  const [wrongGuesses, setWrongGuesses] = useState(0);
 
-    useEffect(() => {
-        if (wrongGuesses >= 3) {
-            // Code to show the popup or message for game over
-            window.alert("Game Over! You made too many wrong guesses.");
-            restartGameFunction();
-        }
-    }, [wrongGuesses]);
+  useEffect(() => {
+    if (wrongGuesses >= 3) {
+      alert("Game Over! You made too many wrong guesses.");
+      restartGameFunction();
+    }
+  }, [wrongGuesses]);
 
-    const letterSelectFunction = (letter) => {
-        if (!chosenLetters.includes(letter)) {
-            setChosenLetters([...chosenLetters, letter]);
-            if (!wordData.word.includes(letter)) {
-                setWrongGuesses(wrongGuesses + 1);
-            }
-        }
-    };
+  const letterSelectFunction = (letter) => {
+    if (!chosenLetters.includes(letter)) {
+      setChosenLetters([...chosenLetters, letter]);
+      if (!wordData.word.includes(letter)) {
+        setWrongGuesses(wrongGuesses + 1);
+      }
+    }
+  };
 
-    const hintFunction = () => {
-        if (hints > 0) {
-            const hiddenLetterIndex = wordData.word
-                .split("")
-                .findIndex((letter) => !chosenLetters.includes(letter));
-            setChosenLetters([...chosenLetters, wordData.word[hiddenLetterIndex]]);
-            setHints(hints - 1);
-        }
-    };
+  const hintFunction = () => {
+    if (hints > 0) {
+      const hiddenLetterIndex = wordData.word
+        .split("")
+        .findIndex((letter) => !chosenLetters.includes(letter));
 
-    const removeCharacterFunction = () => {
-        setChosenLetters(chosenLetters.slice(0, -1));
-    };
+      if (hiddenLetterIndex !== -1) {
+        setChosenLetters([
+          ...chosenLetters,
+          wordData.word[hiddenLetterIndex]
+        ]);
+        setHints(hints - 1);
+      }
+    }
+  };
 
-    const displayLettersFunction = () => {
-        const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const removeCharacterFunction = () => {
+    setChosenLetters(chosenLetters.slice(0, -1));
+  };
 
-        return Array.from(letters).map((letter, index) => (
-            <button
-                key={index}
-                onClick={() => letterSelectFunction(letter)}
-                disabled={chosenLetters.includes(letter)}
-                className={`letter-button ${
-                    chosenLetters.includes(letter) ? "selected" : ""
-                }`}
+  const displayLettersFunction = () => {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    return Array.from(letters).map((letter, index) => (
+      <button
+        key={index}
+        onClick={() => letterSelectFunction(letter)}
+        disabled={chosenLetters.includes(letter)}
+        className={`letter-button ${
+          chosenLetters.includes(letter) ? "selected" : ""
+        }`}
+      >
+        {letter}
+      </button>
+    ));
+  };
+
+  const checkWordGuessedFunction = () => {
+    return wordData.word
+      .split("")
+      .every((letter) => chosenLetters.includes(letter));
+  };
+
+  const guessFunction = () => {
+    if (checkWordGuessedFunction()) {
+      setMsg("Congo Geek! You have guessed the word correctly!");
+    } else {
+      setMsg("You made a Wrong Guess Geek!. Try again!");
+      setDisplayWord(true);
+    }
+  };
+
+  const restartGameFunction = () => {
+    setWordData(getRandomWord());
+    setMsg("");
+    setChosenLetters([]);
+    setHints(3);
+    setDisplayWord(false);
+    setWrongGuesses(0);
+  };
+
+  return (
+    <div className="box">
+      <div className="container">
+        <h1>Mystery Word</h1>
+        <div className="word-container">
+          {Array.from(wordData.word).map((letter, index) => (
+            <div
+              key={index}
+              className={`letter ${
+                chosenLetters.includes(letter) ? "visible" : ""
+              }`}
             >
-                {letter}
+              {chosenLetters.includes(letter) ? letter : ""}
+            </div>
+          ))}
+        </div>
+        <p className="word-description">HINT : {wordData.description}</p>
+        {msg && (
+          <div className="message">
+            <p>{msg}</p>
+            {displayWord && <p>Correct word was: {wordData.word}</p>}
+          </div>
+        )}
+        <div className="button-section">
+          <div className="guess-section">
+            <button
+              onClick={restartGameFunction}
+              className="restart-button"
+            >
+              NEXT
             </button>
-        ));
-    };
-
-    const checkWordGuessedFunction = () => {
-        return wordData.word.split("").every((letter) => chosenLetters.includes(letter));
-    };
-
-    const guessFunction = () => {
-        if (checkWordGuessedFunction()) {
-            setMsg("Congo Geek! You have guessed the word correctly!");
-        } else {
-            setMsg("You made a Wrong Guess Geek!. Try again!");
-            setDisplayWord(true);
-        }
-    };
-
-    const restartGameFunction = () => {
-        setWordData(getRandomWord());
-        setMsg("");
-        setChosenLetters([]);
-        setHints(3);
-        setDisplayWord(false);
-        setGameOver(false);
-        setWrongGuesses(0);
-    };
-
-    return (
-      <div class="box">
-        <div className="container">
-            <h1>Mystery Word</h1>
-            <div className="word-container">
-                {Array.from(wordData.word).map((letter, index) => (
-                    <div
-                        key={index}
-                        className={`letter ${
-                            chosenLetters.includes(letter) ? "visible" : ""
-                        }`}
-                    >
-                        {chosenLetters.includes(letter) ? letter : ""}
-                    </div>
-                ))}
-            </div>
-            <p className="word-description">HINT : {wordData.description}</p>
-            {msg && (
-                <div className="message">
-                    <p>{msg}</p>
-                    {displayWord && <p>Correct word was: {wordData.word}</p>}
-                </div>
-            )}
-            <div className="button-section">
-                <div className="guess-section">
-                    <button
-                        onClick={restartGameFunction}
-                        className="restart-button"
-                    >
-                        NEXT
-                    </button>
-                    <button
-                        onClick={removeCharacterFunction}
-                        disabled={!chosenLetters.length}
-                        className="remove-button"
-                    >
-                        REMOVE LETTER
-                    </button>
-                </div>
-                <div className="letter-selection">
-                    {displayLettersFunction()}
-                </div>
-                <div className="hints">
-                    Hints Remaining : {hints}{" "}
-                    <button
-                        onClick={hintFunction}
-                        disabled={hints === 0}
-                        className="hint-button"
-                    >
-                        Get Hint
-                    </button>
-                </div>
-                {!msg && (
-                    <button
-                        onClick={guessFunction}
-                        disabled={!chosenLetters.length}
-                        className="guess-button"
-                    >
-                        GUESS / SUBMIT
-                    </button>
-                )}
-            </div>
+            <button
+              onClick={removeCharacterFunction}
+              disabled={!chosenLetters.length}
+              className="remove-button"
+            >
+              REMOVE LETTER
+            </button>
+          </div>
+          <div className="letter-selection">
+            {displayLettersFunction()}
+          </div>
+          <div className="hints">
+            Hints Remaining: {hints}{" "}
+            <button
+              onClick={hintFunction}
+              disabled={hints === 0}
+              className="hint-button"
+            >
+              Get Hint
+            </button>
+          </div>
+          {!msg && (
+            <button
+              onClick={guessFunction}
+              disabled={!chosenLetters.length}
+              className="guess-button"
+            >
+              GUESS / SUBMIT
+            </button>
+          )}
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default GFGWordGame;
